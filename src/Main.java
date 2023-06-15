cc63867b38aa08ef067a0b2c3f56a190ee00a8d1
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-06-15 14:30:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13676;
+        int bonusMile = 20;
+
+        int numberMile = ticket / bonusMile;
+        System.out.println(numberMile);
+    }
+}
